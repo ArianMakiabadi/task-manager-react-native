9c47ac21920fb6165c54e866729026d79cc8b381
arian <arian.maki@yahoo.com>
2026-09-04 15:02:51 +0200

    feat: initial input field styles added

--- App.tsx
+++ App.tsx
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="enter new task" style={styles.textInput} />
+        <Button title="submit" />
+      </View>
+      <View>
+        <Text>list of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
+  },
+
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+
+  textInput: {
+    borderWidth: 2,
+    borderColor: "#cccccc",
+    borderRadius: 5,
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
   },
 });
